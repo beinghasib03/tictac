75e7fdcfdf4464853e76028b27f7186d85fbbda7
hasib <hansari@appstec-me.com>
2022-03-30 15:48:58 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import TicTac from './components/TicTac';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Play Game</h3>
+      <TicTac />
+    </div>
+  );
+}
+
+export default App;
